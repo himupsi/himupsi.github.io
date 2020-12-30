a166080fb471177a473aebec7527292873320cca
himupsi <u123u123@naver.com>
2020-12-30 17:53:34 +0900

    sitemap.xml 추가
    자동으로 만들어지게 수정

--- helper/PostsDataMaker.js
+++ helper/PostsDataMaker.js
@@ -1,16 +1,18 @@
 'use strict'
 
+const convert = require('xml-js');
 const fs = require('fs-extra');
 const moment = require('moment');
 const PostParser = require('./PostParser');
 
 const POSTS_PATH = './posts/';
 const DEFAULT_OUTPUT_PATH = './docs/'
+const BLOG_URL = 'https://himupsi.github.io/'
 
 function main()
 {
     let args = process.argv.slice(2);
-    let outputPath = args[0] ? args[0] : DEFAULT_OUTPUT_PATH
+    let outputPath = args[0] ? args[0] : DEFAULT_OUTPUT_PATH;
 
     fs.readdir(POSTS_PATH, function (err, fileNames)
     {
@@ -54,8 +56,69 @@ function main()
         output.categoriesTags['all'] = tags;
         output.posts = posts;
         fs.writeFileSync(outputPath + '/posts.json', JSON.stringify(output, null, '\t'));
+        fs.writeFileSync(outputPath + '/sitemap.xml', createSiteMapXml(BLOG_URL, posts));
     });
 
+}
+
+function createSiteMapXml(defaultUrl, posts)
+{
+    let siteMapObj, url, i;
+
+    url = [
+        {
+            loc: BLOG_URL,
+            changefreq: 'weekly',
+            priority: 0.8,
+            lasmod: moment().format('YYYY-MM-DD')
+        }
+    ];
+    
+    siteMapObj = 
+    {
+        _declaration:
+        {
+            _attributes:
+            {
+                version: "1.0",
+                encoding: "utf-8"
+            }
+        },
+        urlset:
+        {
+            _attributes:
+            {
+                xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9"
+            },
+            url: url
+        }
+    }
+    for (i = 0; i < posts.length; i++)
+    {
+        url.push(createUrlObj(posts[i]))
+    }
+
+    return convert.js2xml(siteMapObj, {compact: true, ignoreComment: true, spaces: 4});
+}
+
+function createUrlObj(post)
+{
+    let urlObj, dateMoment;
+
+
+    urlObj = {
+        loc: BLOG_URL + post.id,
+        changefreq: 'monthly',
+        priority: 0.5
+    };
+
+    if (post.createDate !== undefined)
+    {
+        dateMoment = moment(post.createDate, 'YYYYMMDDhhmm')
+        urlObj.lastmod = dateMoment.format("YYYY-MM-DD") 
+    }
+
+    return urlObj;
 }
 
 main();
